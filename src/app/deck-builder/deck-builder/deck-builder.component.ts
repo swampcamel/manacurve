aaf11b5e63fa49c535d9b0e78f857c68f43e04ec
Eric Conner and Josh Gearheart <eric@>
2018-10-29 13:58:03 -0700

    inject service into deck builder, log to console

--- src/app/deck-builder/deck-builder/deck-builder.component.ts
+++ src/app/deck-builder/deck-builder/deck-builder.component.ts
@@ -1,16 +1,23 @@
 import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
+import { CardService } from '../../card.service';
 
 @Component({
   selector: 'app-deck-builder',
   templateUrl: './deck-builder.component.html',
   styleUrls: ['./deck-builder.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [CardService]
 })
+
 export class DeckBuilderComponent implements OnInit {
   cards = Array.from({length: 100000}).map((_, i) => `Item #${i}`);
-  constructor() { }
   newDeck = [];
+
+  constructor(private cardService: CardService) { }
+
+
   ngOnInit() {
+    console.log('cards are: ' + this.cardService.getCards()); //take this out; this is for testing
   }
 
   addCardToDeck(card){
