950102e47af6582d1864ae58db8454d23253bfe2
Student <example@example.com>
2018-10-29 11:12:50 -0700

    add deck view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+import {ScrollingModule} from '@angular/cdk/scrolling';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DeckBuilderComponent } from './deck-builder/deck-builder/deck-builder.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -14,7 +18,11 @@ import { WelcomeComponent } from './welcome/welcome.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    ScrollingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/deck-builder/deck-builder/deck-builder.component.ts
+++ src/app/deck-builder/deck-builder/deck-builder.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-deck-builder',
   templateUrl: './deck-builder.component.html',
-  styleUrls: ['./deck-builder.component.scss']
+  styleUrls: ['./deck-builder.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DeckBuilderComponent implements OnInit {
-
+  cards = Array.from({length: 100000}).map((_, i) => `Item #${i}`);
   constructor() { }
-
+  newDeck = [];
   ngOnInit() {
   }
 
+  addCardToDeck(card){
+    this.newDeck.push(card);
+    console.log(this.newDeck)
+  }
 }
