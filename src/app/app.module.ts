b2d59bb987b74bf1d8b5634ad1a739cf460bdf33
Eric Conner and Josh Gearheart <josh@>
2018-10-29 10:51:29 -0700

    add api service, connect & init firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { theMasterFirebaseConfig } from './api-keys';
+
+export const firebaseConfig = {
+  apiKey: theMasterFirebaseConfig.apiKey,
+  authDomain: theMasterFirebaseConfig.authDomain,
+  databaseURL: theMasterFirebaseConfig.databaseURL,
+  storageBucket: theMasterFirebaseConfig.storageBucket
+}
 
 @NgModule({
   declarations: [
@@ -10,7 +21,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
