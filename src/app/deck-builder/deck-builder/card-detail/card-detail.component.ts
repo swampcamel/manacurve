8bab53c3dcb93a751f039d47c2a899fba0f12c4a
Student <example@example.com>
2018-10-31 16:45:46 -0700

    mana improvements

--- src/app/deck-builder/deck-builder/card-detail/card-detail.component.ts
+++ src/app/deck-builder/deck-builder/card-detail/card-detail.component.ts
@@ -1,19 +1,36 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-detail',
   templateUrl: './card-detail.component.html',
   styleUrls: ['./card-detail.component.scss']
 })
-export class CardDetailComponent {
+export class CardDetailComponent implements OnInit {
   @Input() selectedCard;
+  manaToDisplay;
   constructor() { }
+
+  ngOnInit() {
+    if(this.selectedCard){
+    this.translateMana(this.selectedCard)}
+  }
+
   testForColor(card) {
     if(card.colors == undefined) {
       return "Grey";
     } else {
       return card.colors[0];
     }
+  }
+
+  translateMana(card) {
+    let splitMana = card.manaCost.split('');
+    splitMana = splitMana.filter(function(value) {
+      if(value != "{" && value !="}") {
+        return value;
+      }
+    })
+    this.manaToDisplay = splitMana;
   }
 
 }
