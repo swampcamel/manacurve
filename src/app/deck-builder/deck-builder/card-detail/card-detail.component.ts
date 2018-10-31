b93e602786d222ba9ea82cb42445c39ac629dbe6
GrumpyEwok <evanfilkins@gmail.com>
2018-10-31 10:27:17 -0700

    Add card-detail component.

---
+++ src/app/deck-builder/deck-builder/card-detail/card-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-detail',
+  templateUrl: './card-detail.component.html',
+  styleUrls: ['./card-detail.component.scss']
+})
+export class CardDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
